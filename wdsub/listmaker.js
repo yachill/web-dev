48386170e4050d031d80292ad00b35b058aa0699
yahill <student.ch@youngsvilleacademy.org>
2026-05-06 08:59:19 -0400

    Update listmaker.js

--- wdsub/listmaker.js
+++ wdsub/listmaker.js
@@ -1,15 +1,19 @@
-const TRASH_BUTTON = " <button class='trash-btn'; onclick='deleteItem(this)'><svg viewBox='0 0 448 512' width='15' title='trash-alt'><path d='M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z' /></svg></button>";
-const CHECK_BOX = "<button onclick='uncheckItem(this)'><svg viewBox='0 0 448 512' width='15' title='check-square' style='fill:gray;'><path d='M400 480H48c-26.51 0-48-21.49-48-48V80c0-26.51 21.49-48 48-48h352c26.51 0 48 21.49 48 48v352c0 26.51-21.49 48-48 48zm-204.686-98.059l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.248-16.379-6.249-22.628 0L184 302.745l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.25 16.379 6.25 22.628.001z'/></svg></button>"
+const TRASH_BUTTON = " <button class='trash-btn' onclick='deleteItem(this)'><svg viewBox='0 0 448 512' width='15' title='trash-alt'><path d='M32 464a48 48 0 0 0 48 48h288a48 48 0 0 0 48-48V128H32zm272-256a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zm-96 0a16 16 0 0 1 32 0v224a16 16 0 0 1-32 0zM432 32H312l-9.4-18.7A24 24 0 0 0 281.1 0H166.8a23.72 23.72 0 0 0-21.4 13.3L136 32H16A16 16 0 0 0 0 48v32a16 16 0 0 0 16 16h416a16 16 0 0 0 16-16V48a16 16 0 0 0-16-16z' /></svg></button>";
+const CHECK_BOX = "<button onclick='uncheckItem(this)'><svg viewBox='0 0 448 512' width='15' title='check-square' style='fill:gray'><path d='M400 480H48c-26.51 0-48-21.49-48-48V80c0-26.51 21.49-48 48-48h352c26.51 0 48 21.49 48 48v352c0 26.51-21.49 48-48 48zm-204.686-98.059l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.248-16.379-6.249-22.628 0L184 302.745l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.25 16.379 6.25 22.628.001z'/></svg></button>"
 const UNCHECK_BOX = "<button onclick= 'checkItem(this)'><svg viewBox='0 0 448 512' width='15' title='square'><path d='M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48z'/></svg></button>"
+
 let inputItem=document.getElementById("input-item");
 let itemList=document.getElementById("item-list");
+
+loadList(); 
+
+document.addEventListener("beforeunload", () => saveList());
 
 document.addEventListener("keydown", (key)=> {
   if (key.code == "Enter") addItem(key);
 });
 
-
-
+//FUNCTION DEFINITIONS
 function addItem(event) {
   if (inputItem.value != "") {
 
@@ -52,3 +56,13 @@ function uncheckItem(elem) {
   parentLI.innerHTML = UNCHECK_BOX + parentLI.innerText+ TRASH_BUTTON;
 
 }
+
+function saveList() {
+  document.cookie = "list=" + itemList.innerHTML;
+}
+
+function loadList() {
+  let oldList = document.cookie.substring(5);
+  if (oldList != "") itemList.innerHTML = oldList;
+}
+  
