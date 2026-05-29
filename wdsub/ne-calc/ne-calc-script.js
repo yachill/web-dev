d98aa30b4dcff74fed7e51f2005872f8213a2a16
yahill <student.ch@youngsvilleacademy.org>
2026-05-29 09:37:51 -0400

    Update ne-calc-script.js

--- wdsub/ne-calc/ne-calc-script.js
+++ wdsub/ne-calc/ne-calc-script.js
@@ -1,7 +1,7 @@
 
 let urlParams = new URLSearchParams(window.location.search);
-const P1_STRATS = urlParams.get("p1Strats");
-const P2_STRATS = urlParams.get("p2Strats");
+const P1_STRATS = urlParams.get("p1Strats")/10;
+const P2_STRATS = urlParams.get("p2Strats")/10;
 
 const PAYOFF_CELL_CONTENTS = "(<input type='number'>, <input type='number'>)";
 
